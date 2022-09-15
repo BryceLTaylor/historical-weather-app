2fbdcded6581aab07d20dbd2986a9361ca8d4329
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-09-15 14:20:27 -0400

    call functions with command line arguments

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('file is running');
+const arguments = process.argv.slice(2);
+
+if (arguments[0] === 'days-of-precip'){
+    precipitation();
+} else if (arguments[0] === 'max-temp-delta') {
+    tempChange();
+} else {
+    console.log('invalid function.  Please use "days-of-precip" or "max-temp-delta" as your first argument');
+}
+
+function precipitation (){
+    console.log('days of precipitation function');
+}
+
+function tempChange (){
+    console.log('greatest temperature change function');
+}
