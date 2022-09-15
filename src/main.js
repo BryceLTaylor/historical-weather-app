0220c9124f1d17be6c80a88921ee864a4a292e05
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-09-15 14:36:23 -0400

    check city argument

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const arguments = process.argv.slice(2);
+let city = getCity(arguments[1]);
 
-if (arguments[0] === 'days-of-precip'){
+if (arguments[0] === 'days-of-precip') {
     precipitation();
 } else if (arguments[0] === 'max-temp-delta') {
     tempChange();
@@ -8,10 +9,28 @@ if (arguments[0] === 'days-of-precip'){
     console.log('invalid function.  Please use "days-of-precip" or "max-temp-delta" as your first argument');
 }
 
-function precipitation (){
-    console.log('days of precipitation function');
+function precipitation () {
+    console.log(`days of precipitation function for city ${city}`);
 }
 
-function tempChange (){
-    console.log('greatest temperature change function');
+function tempChange () {
+    console.log(`greatest temperature change function for city ${city}`);
+}
+
+function getCity (cityName) {
+    switch (cityName){
+        case 'bos':
+            return 'bos';
+            break;
+        case 'jnu':
+            return 'jnu';
+            break;
+        case 'mia':
+            return 'mia';
+            break;
+        default:
+            throw 'please include a valid city, bos, jnu, or mia, as the second argument';
+            break;
+
+    }
 }
